c2dddbaaf9a1f4c7bd8e46ea583a370c600d0b6a
Odonno <bottiau.david@laposte.net>
2015-12-14 23:53:55 +0100

    feat(oauth): execute OAuth of Gitter API

--- js/default.js
+++ js/default.js
@@ -3,34 +3,36 @@
 (function () {
     "use strict";
 
-    angular.module('modern-gitter', ['winjs'])
-    .controller('RoomsCtrl', function ($scope) {
-        // properties
-        $scope.rooms = [{
-            name: "Modern-Gitter"
-        }]
-    });
-
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize your application here.
+                // TODO : This application has been newly launched, initialize your application here
+                let oauth = new TimmyTools.oauth();
+
+                oauth.connect().then(t => {
+                    console.log('Sucessfully logged to Gitter API');
+                });
             } else {
-                // TODO: This application was suspended and then terminated.
-                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+                // TODO : This application was suspended and then terminated
             }
             args.setPromise(WinJS.UI.processAll());
         }
     };
 
     app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
-        // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
-        // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+        // TODO : This application is about to be suspended, save any state that needs to persist across suspensions here
     };
 
     app.start();
+
+    angular.module('modern-gitter', ['winjs'])
+        .controller('RoomsCtrl', function ($scope) {
+            // properties
+            $scope.rooms = [{
+                name: "Modern-Gitter"
+            }]
+        });
 })();
---
+++ js/oauth.js
@@ -0,0 +1,115 @@
+﻿/// <reference path="../node_modules/winjs/js/base.js" />
+/// <reference path="../node_modules/winjs/js/ui.js" />
+
+(function () {
+    WinJS.Namespace.define("TimmyTools", {
+        oauth: WinJS.Class.define(
+            function () {
+                this.refreshToken = retrieveTokenFromVault();
+            },
+            {
+                connect: function () {
+                    return new Promise((done, error) => {
+                        if (!this.refreshToken) {
+                            authenticate().then(
+                                token => grant(token).then(accessToken => {
+                                    let cred = new Windows.Security.Credentials
+                                        .PasswordCredential("OauthToken", "CurrentUser", accessToken.access_token);
+                                    this.refreshToken = accessToken.access_token;
+                                    let passwordVault = new Windows.Security.Credentials.PasswordVault();
+                                    passwordVault.add(cred);
+                                    done(this.refreshToken);
+                                }));
+                        } else {
+                            done(this.refreshToken);
+                        }
+                    });
+                }
+            }
+        )
+    });
+
+    function retrieveTokenFromVault() {
+        let passwordVault = new Windows.Security.Credentials.PasswordVault();
+        let storedToken;
+
+        try {
+            let credential = passwordVault.retrieve("OauthToken", "CurrentUser");
+            storedToken = credential.password;
+            // Uncomment this line to remove the token from the password vault so you'll have to log in again
+            //passwordVault.remove(credential);
+        } catch (e) {
+            // no stored credentials
+        }
+
+        return storedToken;
+    }
+
+    function grant(token) {
+        let oauthUrl = gitterConfig.token_uri;
+        let clientId = gitterConfig.client_id;
+        let clientSecret = gitterConfig.client_secret;
+        let redirectUrl = gitterConfig.redirect_uris[0];
+
+        return WinJS.xhr({
+            type: "post",
+            url: oauthUrl,
+            data: serializeData({
+                code: token,
+                client_id: clientId,
+                client_secret: clientSecret,
+                redirect_uri: redirectUrl,
+                grant_type: 'authorization_code'
+            }),
+            headers:
+            {
+                "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+            }
+        }).then(x => JSON.parse(x.response));
+    };
+
+    function authenticate() {
+        return new Promise(function (complete, error) {
+            let oauthUrl = gitterConfig.auth_uri;
+            let clientId = gitterConfig.client_id;
+            let redirectUrl = gitterConfig.redirect_uris[0];
+            let requestUri = Windows.Foundation.Uri(`${oauthUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUrl)}&response_type=code&access_type=offline`);
+            let callbackUri = Windows.Foundation.Uri(redirectUrl);
+
+            Windows.Security.Authentication.Web.WebAuthenticationBroker.
+                authenticateAsync(Windows.Security.Authentication.Web.
+                    WebAuthenticationOptions.none, requestUri, callbackUri)
+                .done(result => {
+                    if (result.responseStatus === 0) {
+                        complete(result.responseData.replace('http://localhost/?code=', ''));
+                    } else {
+                        error(result);
+                    }
+                });
+        });
+    }
+
+    /// Serialize a piece of data to a querystring
+    function serializeData(data, encode) {
+        if (typeof data !== 'object') {
+            return ((data == null) ? "" : data.toString());
+        }
+        let buffer = [];
+
+        // Serialize each key in the object
+        for (let name in data) {
+            if (!data.hasOwnProperty(name)) {
+                continue;
+            }
+            let value = data[name];
+            if (!!encode) {
+                buffer.push(`${encodeURIComponent(name)} = ${encodeURIComponent((value == null) ? "" : value)}`);
+            } else {
+                buffer.push(`${name}=${value == null ? "" : value}`);
+            }
+        }
+
+        // Serialize the buffer and clean it up for transportation
+        return buffer.join("&").replace(/%20/g, "+");
+    }
+})();
