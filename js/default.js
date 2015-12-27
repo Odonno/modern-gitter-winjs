e1ae747c723b64020c213343aff262c5a72da97a
Odonno <bottiau.david@laposte.net>
2015-12-27 23:36:40 +0100

    feat(actions): send message when pressing enter key

--- js/default.js
+++ js/default.js
@@ -38,6 +38,19 @@
     app.start();
 
     angular.module('modern-gitter', ['winjs', 'ngSanitize'])
+        .directive('ngEnter', function () {
+            return function (scope, element, attrs) {
+                element.bind("keydown keypress", function (event) {
+                    if (event.which === 13) {
+                        scope.$apply(function () {
+                            scope.$eval(attrs.ngEnter);
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            };
+        })
         .service('NetworkService', function () {
             var networkService = this;
 
