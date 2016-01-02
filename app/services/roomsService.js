6e7045cfc4ccd9e4c79f9ea024fe67b1e9966dee
Odonno <bottiau.david@laposte.net>
2016-01-02 20:24:18 +0100

    refactor(roomsService): create method addRoom

--- app/services/roomsService.js
+++ app/services/roomsService.js
@@ -6,7 +6,27 @@ angular.module('modern-gitter')
         roomsService.initialized = false;
         roomsService.rooms = [];
 
-        // methods
+        // private methods
+        function addRoom(room) {
+            // compute room image
+            if (room.user) {
+                room.image = room.user.avatarUrlMedium;
+            } else {
+                room.image = "https://avatars.githubusercontent.com/" + room.name.split('/')[0];
+            }
+                
+            // subscribe to realtime messages
+            RealtimeApiService.subscribe(room.id, function (roomId, message) {
+                if (roomsService.onmessagereceived) {
+                    roomsService.onmessagereceived(roomId, message);
+                }
+                // TODO : send notification
+            });
+
+            roomsService.rooms.push(room);
+        }
+
+        // public methods
         roomsService.initialize = function () {
             OAuthService.connect().then(t => {
                 console.log('Sucessfully logged to Gitter API');
@@ -15,33 +35,14 @@ angular.module('modern-gitter')
                     console.log('Sucessfully subscribed to realtime API');
 
                     ApiService.getRooms().then(rooms => {
-                        roomsService.setRooms(rooms);
+                        for (var i = 0; i < rooms.length; i++) {
+                            addRoom(rooms[i]);
+                        }
                         roomsService.initialized = true;
                     });
                 });
             });
         }
-
-        roomsService.setRooms = function (rooms) {
-            roomsService.rooms = rooms;
-
-            for (var i = 0; i < roomsService.rooms.length; i++) {
-                // compute room image
-                if (roomsService.rooms[i].user) {
-                    roomsService.rooms[i].image = roomsService.rooms[i].user.avatarUrlMedium;
-                } else {
-                    roomsService.rooms[i].image = "https://avatars.githubusercontent.com/" + roomsService.rooms[i].name.split('/')[0];
-                }
-
-                // subscribe to realtime messages
-                RealtimeApiService.subscribe(roomsService.rooms[i].id, function (roomId, message) {
-                    if (roomsService.onmessagereceived) {
-                        roomsService.onmessagereceived(roomId, message);
-                    }
-                    // TODO : send notification
-                });
-            }
-        };
 
         roomsService.selectRoom = function (room) {
             roomsService.currentRoom = room;
@@ -52,22 +53,7 @@ angular.module('modern-gitter')
 
         roomsService.createRoom = function (name, callback) {
             ApiService.joinRoom(name).then(room => {
-                // compute room image
-                if (room.user) {
-                    room.image = room.user.avatarUrlMedium;
-                } else {
-                    room.image = "https://avatars.githubusercontent.com/" + room.name.split('/')[0];
-                }
-                
-                // subscribe to realtime messages
-                RealtimeApiService.subscribe(room.id, function (roomId, message) {
-                    if (roomsService.onmessagereceived) {
-                        roomsService.onmessagereceived(roomId, message);
-                    }
-                    // TODO : send notification
-                });
-                
-                roomsService.rooms.push(room);
+                addRoom(room);
                 callback(room);
             });
         };
