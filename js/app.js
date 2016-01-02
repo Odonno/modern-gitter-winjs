7e9ad9c2bf2cb185a686437dcf1122215d8e2995
Odonno <bottiau.david@laposte.net>
2016-01-02 03:56:01 +0100

    design(addRoom): design channel room creation

--- js/app.js
+++ js/app.js
@@ -26,8 +26,19 @@
     });
 angular.module('modern-gitter')
     .controller('AddRoomCtrl', function ($scope, $filter, ApiService) {
+        // properties
+        $scope.owners = [];
+        $scope.channel = {};
+        
+        // methods
+        $scope.selectOwner = function (owner) {
+            $scope.channel.owner = owner;  
+        };
+        
         // initialize controller
         ApiService.getCurrentUser().then(function (user) {
+            $scope.owners.push(user);
+
             ApiService.getRepositories(user.id).then(function (repositories) {
                 $scope.repositories = repositories;
             });
